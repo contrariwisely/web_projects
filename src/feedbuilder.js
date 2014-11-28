7ecc10ed04e7bca1923a6c669cc9ed431d12a1b8
contrariwisely <jessicapalmer99@gmail.com>
2014-11-28 15:13:32 -0500

    basic code structure- doesn't display anything yet

--- src/feedbuilder.js
+++ src/feedbuilder.js
@@ -1,28 +1,49 @@
 var main = function(){
-	//set feed info
-	var feed_url = "";
-	var feed_num = "";
-	var $feed_list = $(<div class=feed_list><ul></ul</div>).
-	$('.rssfeed').append(feed_list);
-	
-	//initialize API
-	google.load("feeds", "1");
-	  
-		function initialize() {
-			var feed = new google.feeds.Feed(feed_url);
-			feed.setNumEntries(feed_num);
-			feed.load(function(result) {
-				if (!result.error) {
-					var container = document.getElementById("feed");
-					for (var i = 0; i < result.feed.entries.length; i++) {
-						var entry = result.feed.entries[i];
-						var div = document.createElement("div");
-						div.appendChild(document.createTextNode(entry.title));
-						container.appendChild(div);
-					}
-				}
-		});
-	}
+	//initialize Google Feed API
+	feed.load(function(result) {
+	  if (!result.error) {
+		var container = document.getElementsByClassName("rssfeed");
+		for (var x = 0; x < container.length; x++) {
+			var feed_url = container[x].rss_url;
+			var feed_num = container[x].rss_num;	
+			var feed_title = container[x].rss_title;
+
+			//add linked title to container
+			var title_link = '<h2><a href = "' + feed_url + '" target = _blank">' + feed title + '</a></h2>';
+			container[x].innerHTML = title_link;
+			
+			//create list and description containers
+			var feed_list = document.createElement("div");
+			feed_list.className = "feed_list";
+			feed_list.innerHTML = '<ul></ul>';
+			container[x].appendChild(feed_list);
+			
+			var feed_desc = document.createElement("div");
+			feed_desc.className = "feed_desc";
+			container[x].appendChild(feed_desc);
+					
+			//get feed 
+			for (var i = 0; i < result.feed.entries.length; i++) {
+			  var entry = result.feed.entries[i];
+			  var li = document.createElement("li");
+			  li.innerHTML = '<h3>' + entry.title + '</h3>';
+			  feed_list.appendChild(li);
+			  var div = document.createElement("div");
+			  div.innerHTML = '<h3> <a href="' + entry.link + '">' + entry.title + '</a>' + '</h3>' + '<p>' + entry.contentSnippet + '</p>';
+			  feed_desc.appendChild(div);
+			}
+		} 
+	  }else {
+		var container = document.getElementsByClassName("rssfeed");
+		for (var x = 0; x < container.length; x++) {
+			var feed_title = document.createElement('h2');
+			feed_title.innerHTML = container[x].rss_title;
+			var feed_url = document.createElement('h3');
+			feed_url.innerHTML = '<a href="' + container[x].rss_url + '">Get the latest</a></li>';
+			container[x].appendChild(feed_url);
+		}
+	  };
+	});
 };
 
 $(document).ready(main);
